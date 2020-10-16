50c54f05765547fdd802551aeba346493bcd9a02
LeVinhHao <50310846+Haokaka96@users.noreply.github.com>
2020-10-16 16:58:34 +0700

    AddAuthorHeader

--- UserManagerNetCore/Startup.cs
+++ UserManagerNetCore/Startup.cs
@@ -1,9 +1,11 @@
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 using UserManagerNetCore.Infrastructure.Extensions;
 
 namespace UserManagerNetCore
@@ -23,7 +25,52 @@ namespace UserManagerNetCore
                        .AddIdentity()
                        .AddJwtAuthentication(services.GetApplicationSettings(this.Configuration))
                        .AddApplicationServices()
-                       .AddSwagger()
+                       .AddSwaggerGen(s =>
+                       {
+                           s.SwaggerDoc("v1", new OpenApiInfo
+                           {
+                               Version = "v1",
+                               Title = "User management",
+                               Description = "My Api",
+                               Contact = new OpenApiContact
+                               {
+                                   Name = "Lê Vĩnh Hảo",
+                                   Email = "vinhhao2604@gmail.com",
+                                   Url = new Uri("https://Fb.com/haole2604")
+                               },
+                               License = new OpenApiLicense
+                               {
+                                   Name = "MIT",
+                                   Url = new Uri("https://github.com/ignaciojvig/ChatAPI/blob/master/LICENSE")
+                               }
+
+                           });
+
+                           s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                           {
+                               Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                               Name = "Authorization",
+                               In = ParameterLocation.Header,
+                               Type = SecuritySchemeType.ApiKey,
+                               Scheme = "Bearer"
+                           });
+
+                           s.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+                       })
                        .AddAutoMapper(typeof(AutoMapperProfile).Assembly)
             .AddControllers().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
@@ -49,10 +96,12 @@ namespace UserManagerNetCore
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwagger();
 
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                
             });
 
             app.UseEndpoints(endpoints =>
