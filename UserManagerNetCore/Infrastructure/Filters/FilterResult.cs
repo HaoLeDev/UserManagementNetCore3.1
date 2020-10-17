e0f5c88d305af433aeaeb16b0964c80d71456157
LeVinhHao <50310846+Haokaka96@users.noreply.github.com>
2020-10-17 14:46:44 +0700

    filter result

---
+++ UserManagerNetCore/Infrastructure/Filters/FilterResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagerNetCore.Infrastructure.Filters
+{
+    public class FilterResult<T>
+    {
+        public FilterResult(T data, int statusCode, string[]error)
+        {
+            Data = data;
+            this.statusCode = statusCode;
+            this.Errors = error;
+        }
+
+
+        public bool Succeeded {
+            get {
+                if (Errors != null) return false;
+                else return true;} 
+        }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+        public int statusCode { get; set; }
+        public T Data { get; set; }
+    }
+}
